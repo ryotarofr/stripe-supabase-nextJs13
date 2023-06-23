444322646d2d31019541583b5aa6cff2f3e51e67
ryotarofr <ryoryo.fr0608@icloud.com>
2023-06-23 10:52:05 +0900

    🎉 initial commit

---
+++ app/layout.tsx
@@ -0,0 +1,21 @@
+import './globals.css'
+import { Figtree } from 'next/font/google'
+
+const font = Figtree({ subsets: ['latin'] })
+
+export const metadata = {
+  title: 'Spotify Clone is change for me',
+  description: 'Welcome to Learn',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={font.className}>{children}</body>
+    </html>
+  )
+}
